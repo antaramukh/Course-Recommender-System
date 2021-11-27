46b691ef51dbc0b95d6baa4c2dc3920e98fd69ce
Antara Mukherjee <82995717+antaramukh@users.noreply.github.com>
2021-11-28 02:03:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,31 +2,30 @@ from flask import Flask, render_template, request
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 app = Flask(__name__)
 dataset = pd.read_csv('courses.csv')
 
 
 def vectorize_text_to_cosine_mat(data):
-    count_vect = CountVectorizer()
-    cv_mat = count_vect.fit_transform(data)
-    # Get the cosine
-    cosine_sim_mat = cosine_similarity(cv_mat)
-    return cosine_sim_mat
+    vectorizer = TfidfVectorizer(max_features=1000)
+    course_vectors = vectorizer.fit_transform(data.values)
+    similarity = cosine_similarity(course_vectors)
+    return similarity
 
 
-def get_recommendation(title, cosine_sim_mat, df, num_of_rec):
-    title=title.lower()
-    course_indices = pd.Series(df.index, index=df['course_title']).drop_duplicates()
-    idx = course_indices[title]
-    scores = list(enumerate(cosine_sim_mat[idx]))
+def get_recommendation(title, similarity, df, num_of_rec):
+    title = title.lower()
+    course_index = df[df['Name'] == title].index[0]
+    scores = list(enumerate(similarity[course_index]))
     sorted_scores = sorted(scores, key=lambda x: x[1], reverse=True)
     selected_course_indices = [i[0] for i in sorted_scores[1:]]
     selected_course_scores = [i[1] for i in sorted_scores[1:]]
-    result = df[['course_title', 'url']].iloc[selected_course_indices]
+    result = df[['Name', 'Url']].iloc[selected_course_indices]
     rec_df = pd.DataFrame(result)
     rec_df['similarity_scores'] = selected_course_scores
-    rec_df['course_title']=rec_df['course_title'].str.title()
+    rec_df['Name'] = rec_df['Name'].str.title()
     return rec_df.head(num_of_rec)
 
 
@@ -38,12 +37,14 @@ def index():
 @app.route('/result', methods=['POST'])
 def result():
     df = pd.read_csv('courses.csv')
-    cosine_sim_mat = vectorize_text_to_cosine_mat(df['course_title'])
+    df = df.iloc[:,1:]
 
-    search_term = str(request.form.get('age'))
-    num_of_rec = int(request.form.get('sex'))
+    search_term = str(request.form.get('title'))
+    num_of_rec = int(request.form.get('num'))
 
     try:
+        cosine_sim_mat = vectorize_text_to_cosine_mat(df['content'])
+
         data = get_recommendation(search_term, cosine_sim_mat, df, num_of_rec)
 
         data = data.rename(columns={'course_title': 'Course Name', 'url': 'Course Link', 'similarity_scores': 'Similarity Score'})
